8f3d748affdefc7d36d702483ab6e33226999707
Arjun Vaskale <arjun@Arjuns-MacBook-Pro.local>
2022-01-08 14:45:36 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export interface Props {
+  name: string;
+  baseEnthusiasmLevel?: number;
+};
+
+const Hello: React.FC<Props> = ({
+  name = 'Dudu',
+  baseEnthusiasmLevel = 0
+}) => {
+  const [enthusiasmLevel, setEnthusiasmLevel] = React.useState(
+    baseEnthusiasmLevel
+  );
+
+  const onIncrement = () =>
+    setEnthusiasmLevel(enthusiasmLevel + 1);
+  const onDecrement = () =>
+    setEnthusiasmLevel(
+      enthusiasmLevel > 0 ? enthusiasmLevel - 1 : 0
+    );
+
+  const getExclamationMarks = (numChars: number) =>
+    numChars > 0 ? Array(numChars + 1).join('!') : '';
+
+
+    const MyName = () => 
+(<View><Text>my name is Arjun Vaskale</Text>
+  <Text>another name  with function ,,,</Text></View>)
+
+    
+     
+      
+
+
+  return (
+    <View style={styles.container}>
+{MyName()}
+      <Text style={styles.greeting}>
+        Hello {name}
+        {getExclamationMarks(enthusiasmLevel)}
+      </Text>
+      <View>
+        <Button
+          title="Increase enthusiasm"
+          accessibilityLabel="increment"
+          onPress={onIncrement}
+          color="blue"
+        />
+        <Button
+          title="Decrease enthusiasm"
+          accessibilityLabel="decrement"
+          onPress={onDecrement}
+          color="red"
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  greeting: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 16
+  }
+});
+
+export default Hello;
